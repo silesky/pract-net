140072a45162370dcea7c6d134789b8cf04bb64e
Me <me@myhouse.com>
2016-08-21 15:04:55 -0500

    add seeding stuff

--- Data/PracticeTimerContextSeeder.cs
+++ Data/PracticeTimerContextSeeder.cs
@@ -7,9 +7,11 @@ namespace PracticeTimer.Data {
     public static class PracticeTimerContextSeeder {
         public static void Seed(PracticeTimerContext context) {
             var timers = new List<Timer> {
-                new Timer{Id = Guid.NewGuid(), Title = "", StartTime = 20, }
+                new Timer{Id = Guid.NewGuid(), Time=2, Title = "eat", Ticking = false, StartTime = 20, Paused = true},
+                new Timer{Id = Guid.NewGuid(), Time=4, Title = "love", Ticking = false, StartTime = 20, Paused = true},
+                new Timer{Id = Guid.NewGuid(), Time=6, Title = "pray", Ticking = false, StartTime = 20, Paused = true}
             };
-
+        
             foreach (var t in timers) context.Timers.Add(t);
             context.SaveChanges();
              
