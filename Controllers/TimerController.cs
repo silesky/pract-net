69de9bce4e06cb9fa9320e7b68034fbaa125d334
Seth Silesky <seth.silesky@gmail.com>
2016-08-28 12:33:14 -0500

    fix routing
    http://localhost:5000/api/timer/f46020bc-e1c0-4225-a04f-20415156b3a5

--- Controllers/TimerController.cs
+++ Controllers/TimerController.cs
@@ -7,27 +7,27 @@ using PracticeTimer.DataTransferObjects;
 namespace PracticeTimer.Controllers {
     [Route("api/[controller]")]
     public class TimerController : PracticeTimerController {
-        
-        [HttpGet("{id}")]
+
+    
+
+        [HttpGet("{id:guid}")]
         public IActionResult GetTimer(Guid id) {
             var timer = Context.Timers.FirstOrDefault(t => t.Id == id);
-            
 
+            Console.WriteLine("get individual timer hit!" );
             return new ObjectResult(timer);
         }
-
-        [HttpGet("")]
+    [HttpGet("")]
         public IActionResult GetAll () {
-            Console.WriteLine("hit get all timers");
+            Console.WriteLine("# hit get all timers");
             var timers = Context.Timers.ToList();
-            
+
             return new ObjectResult(timers);
         }
-
         [HttpPut("")]
         public IActionResult Create (TimerDto dto) {
                 // when you hit the put route, you get {success: true} if it suceeded
-                
+                // http://localhost:5000/api/timer/f46020bc-e1c0-4225-a04f-20415156b3a5
                 var timerEntity = new Data.Entities.Timer();
                 // private variables are camelCase
                 timerEntity.Id = dto.Id; //new Guide() would create a blank guid
@@ -37,11 +37,11 @@ namespace PracticeTimer.Controllers {
                 timerEntity.TimerGroupId = dto.TimerGroupId;
                 timerEntity.Paused = dto.Paused;
                 Context.Timers.Add(timerEntity);
-                Context.SaveChanges();    
+                Context.SaveChanges();
                 return new ObjectResult(new {success=true});
         }
 
-        [HttpDelete("{id}")]
+        [HttpDelete("{id:guid}")]
         public IActionResult Delete(Guid id) {
             var entity = Context.Timers.First(t => t.Id == id);
 
@@ -50,7 +50,7 @@ namespace PracticeTimer.Controllers {
 
             return new ObjectResult(new {Success = true});
         }
-        
+
 
     }
 }
