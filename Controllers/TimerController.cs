6f0d736549cfbc940f25b513ca8128a6441ecb0b
Steve Price <steveprice@mooserooster.com>
2016-08-28 15:41:18 -0500

    fix Timer creation route

--- Controllers/TimerController.cs
+++ Controllers/TimerController.cs
@@ -10,16 +10,16 @@ namespace PracticeTimer.Controllers
     public class TimerController : PracticeTimerController
     {
 
-
-
         [HttpGet("{id:guid}")]
         public IActionResult GetTimer(Guid id)
         {
             var timer = Context.Timers.FirstOrDefault(t => t.Id == id);
             var timerDto = toDto(timer);
             Console.WriteLine("get individual timer hit!");
+            
             return new ObjectResult(timerDto);
         }
+
         [HttpGet("")]
         public IActionResult GetAll()
         {
@@ -28,7 +28,6 @@ namespace PracticeTimer.Controllers
 
             return new ObjectResult(timers);
         }
-
 
         private TimerDto toDto(Timer timerEntity) {
             var dto = new TimerDto();
@@ -44,22 +43,30 @@ namespace PracticeTimer.Controllers
         } 
         // TimerDto is found in the bottom of the http request
 
-        [HttpPost]
+        private Timer toEntity(TimerDto dto, Timer savedEntity = null) {
+            var entity = (savedEntity != null) ? savedEntity:  new Timer();
+            if (entity.Id == Guid.Empty) entity.Id = dto.Id;
+            entity.Order = dto.Order;
+            entity.Paused = dto.Paused;
+            entity.Time = dto.Time;
+            entity.Ticking = dto.Ticking;
+            entity.Title = dto.Title;
+            entity.TimerGroup = Context.TimerGroups.FirstOrDefault(tg => tg.Id == dto.TimerGroupId);
+            entity.StartTime = dto.StartTime;
+            return entity;
+        } 
+
+        [HttpPost("")]
         public IActionResult Create([FromBody] TimerDto dto)
         {
             // when you hit the put route, you get {success: true} if it suceeded
             // http://localhost:5000/api/timer/f46020bc-e1c0-4225-a04f-20415156b3a5
-            var timerEntity = new Data.Entities.Timer();
-            // private variables are amelCase
-            timerEntity.Id = dto.Id; //new Guide() would create a blank guid
-            timerEntity.Time = dto.Time;
-            timerEntity.Ticking = dto.Ticking;
-            timerEntity.Title = dto.Title;
-            timerEntity.TimerGroupId = dto.TimerGroupId;
-            timerEntity.Paused = dto.Paused;
-            timerEntity.StartTime = dto.StartTime;
+            // private variables are camelCase
+            var timerEntity = toEntity(dto);
+            
             Context.Timers.Add(timerEntity);
             Context.SaveChanges();
+            
             return CreatedAtRoute("", new { sucess = true }, timerEntity);
         }
 
@@ -73,16 +80,16 @@ namespace PracticeTimer.Controllers
 
             return new ObjectResult(new { Success = true });
         }
-        [HttpPost("{id:guid}")]
-        public IActionResult Update(Guid id)
-        {
-            var entity = Context.Timers.First(t => t.Id == id);
 
+        [HttpPost("{id:guid}")]
+        public IActionResult Update([FromBody] TimerDto dto)
+        {
+            var entity = Context.Timers.First(t => t.Id == dto.Id);
+            
+            entity = toEntity(dto, entity);
             Context.SaveChanges();
 
             return new ObjectResult(new { Success = true });
         }
-
-
     }
 }
