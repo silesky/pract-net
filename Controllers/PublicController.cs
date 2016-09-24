4b38be0be0285333b8b0e2f029895404c4dbbf30
Steve Price <steveprice@mooserooster.com>
2016-09-24 12:24:09 -0500

    fix default index view

--- Controllers/PublicController.cs
+++ Controllers/PublicController.cs
@@ -2,7 +2,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace PracticeTimer.Controllers {
 
+    [Route("/")]
     public class PublicController : PracticeTimerController {
+
+        [HttpGet]
+        public IActionResult Index() {
+            return View("Index");
+        }
 
         // [HttpGet("")]
         // public IActionResult Login() {
--- Startup.cs
+++ Startup.cs
@@ -110,7 +110,7 @@ namespace WebApplication
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Public}/{action=Index}/{id?}");
             });
         }
     }
