b93965ef2691c2f12928a500fde488bb0669c1f7
silesky <seth.silesky@gmail.com>
2016-09-27 11:55:24 -0500

    title shows up now

--- wwwroot/app/components/TimerBoxTitle.js
+++ wwwroot/app/components/TimerBoxTitle.js
@@ -1,24 +1,16 @@
 import React from 'react';
 
 const TimerBoxTitle = ({onTimerBoxTitleSet, eachTitle }) => {
-	let titleSetInput;
+	let titleSetInput; 
 	return (
-
 		<div className="TimerBoxTitle-container">
-			{/* <div className="TimerBoxTitle-text">
-				{eachTitle}
-			</div> */}
 			<input className="TimerBoxTitle-input mdl-textfield__input" size='10' type="text" placeholder="Title"
-				ref={
-					node => {titleSetInput = node;}
-				}
-				onChange={() => {
-					onTimerBoxTitleSet(titleSetInput);
-				}} />
-
+				value={eachTitle || ''}
+				ref={node => {let titleSetInput = node;}}
+				onChange={() => {onTimerBoxTitleSet(titleSetInput); }} 
+			/>
 			</div>
 		);
 	};
-
 
 	export default TimerBoxTitle;
